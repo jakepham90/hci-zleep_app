9f689bb773977aaddf1a854d2d0face009474f11
Alice Nguyen <a8nguyen@ucsd.edu>
2014-02-06 01:46:23 -0800

    create alarm part is done

---
+++ js/zleep.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ /* $(".btn-create-new").click(function(){
+    $(".list-group").prepend(test);
+  });*/
+
+});
+
+
+/*=================================================*/
+var test = "<a href='#' class='list-group-item alarm-item new' > hi</a>";
+
+
+
+
+
