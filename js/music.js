999387f208e9c781e9ff82fe8f32283e229cf109
Nick Variz <nicholas.variz@service-now.com>
2014-02-09 16:11:28 -0800

    Added base music functionality to sleepyJams page

--- js/music.js
+++ js/music.js
@@ -15,10 +15,68 @@ function initializePage() {
 
 function playMusic(e){
 	event.preventDefault();
-	var vol = $("#volume").val();
+
+	var vol = ($("#volume").val()) / 10;
 	var fade = $("#timer").val();
 	var countdown = $("#timer").val();
 	var type = $("#musicpicker :selected").text();
+	
+	var endTime = new Date().getTime() + countdown*60*1000; 
+
+	//Handle undefined values
+	if(fade == ""){
+		console.log("Assuming default fade time of 5 minutes");
+		fade = 5;
+	}
+	if(countdown == ""){
+		console.log("Assuming default countdown time of 30 minutes");
+		countdown = 30;
+	}
+
+	var zenAudio = new Audio('zen.mp3');
+	var natureAudio = new Audio('nature.mp3');
+	var animalAudio = new Audio('animals.mp3');
+
+	//Start playing music with chosen settings
+
+	if (type == "Zen Mix"){
+		console.log("Zen music picked");
+		if(!(natureAudio.ended))
+			natureAudio.pause();
+		if(!(animalAudio.ended))
+			animalAudio.pause();
+		zenAudio.load();
+		zenAudio.volume = vol;
+		zenAudio.loop = true;	
+		zenAudio.play();
+
+	}else if(type == "Nature Sounds"){
+		console.log("Nature Sounds picked");
+		if(!(zenAudio.ended))
+			zenAudio.pause();
+		if(!(animalAudio.ended))
+			animalAudio.pause();
+		natureAudio.load();
+		natureAudio.volume = vol;
+		natureAudio.loop = true;	
+		natureAudio.play();
+
+	}else if(type == "Forest Animals"){
+		console.log("Forest Animals picked");
+		if(!(zenAudio.ended))
+			zenAudio.pause();
+		if(!(natureAudio.ended))
+			natureAudio.pause();
+		animalAudio.load();
+		animalAudio.volume = vol;
+		animalAudio.loop = true;	
+		animalAudio.play();
+	}else{
+		type = "no music";
+		alert("Please select some music from the picker.");
+	}
+	
 	console.log("Playing " + type + ", volume is " + vol + ", timer set to " + countdown + ", fade last "
-		+ countdown + " minutes." );
+		+ fade + " minutes." );
+	
 }
