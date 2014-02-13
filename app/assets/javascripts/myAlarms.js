655f8e82d450fcb13caf5c96509f4d8cfaf79539
Alice Nguyen <a8nguyen@ucsd.edu>
2014-02-13 01:58:49 -0800

    removed hiding function

--- app/assets/javascripts/myAlarms.js
+++ app/assets/javascripts/myAlarms.js
@@ -1,15 +1,8 @@
 (function() {
 
   $(document).ready(function(){
-    console.log('test!!!');
-    hideNewTimes();
   });
 
 }).call(this);
 
 
-function hideNewTimes () {
-
-  $(".calculated-times").css("visibility", "hidden");
-
-}
