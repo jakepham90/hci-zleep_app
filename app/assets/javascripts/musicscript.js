19336542805a270f108531b20f959372b27313a6
Nick Variz <nicholas.variz@service-now.com>
2014-02-16 15:19:34 -0800

    Fixed layout on music player page, made countdown timer and playbutton dynamic. Hide instructions when playback starts

--- app/assets/javascripts/musicscript.js
+++ app/assets/javascripts/musicscript.js
@@ -10,39 +10,49 @@ $(document).ready(function() {
  */
 function initializePage() {
 	timerEventHandler.elapsedTime = 0;
-	document.getElementById("title").innerHTML = "Trouble sleeping?";
-	document.getElementById("timeoff").innerHTML = "Pick a track from below and set the timer!" 	;	
+	document.getElementById("title1").innerHTML = "Trouble sleeping?";
+	document.getElementById("title2").innerHTML = "Pick a track from below and set the timer!" 	;	
 	//register event handlers for volume and play button
 	$("#playButton").click(startTimer);
 
 }
 
 startTimer.userTime;
+startTimer.player;
 
 //Handle countdown timer changes
 function startTimer(e){
 	event.preventDefault();
+
 	startTimer.userTime = $("#timer").val();
-	document.getElementById("title").innerHTML = "Your music will turn off in :";
-	document.getElementById("timeoff").innerHTML = startTimer.userTime + " minutes ";
-	console.log("playbutton pushed, music will play for " + startTimer.userTime + " minutes.");
+	document.getElementById("timerText1").innerHTML = "Your music will turn off in :";
+	document.getElementById("timerText2").innerHTML = startTimer.userTime;
+	console.log("play pushed, music will play for " + startTimer.userTime);
 	window.setInterval(timerEventHandler, 60000);
+
+	//Hide instructions after user initiates playback
+	document.getElementById("title1").style.display = 'none';
+	document.getElementById("title2").style.display = 'none';
+	document.getElementById("playButton").innerHTML = "Restart Timer";
 
 }
 
 //TimerHandler's persistent time property
 timerEventHandler.elapsedTime;
 
-//Every 60 seconds, update the elapse time and fade volume as necessary
+//Every 60 seconds, update the elapse time
 function timerEventHandler(e){
 
 	//Increment elapsed time
 	timerEventHandler.elapsedTime = timerEventHandler.elapsedTime + 1;
 	console.log("Elapsed time: " + timerEventHandler.elapsedTime);
-	document.getElementById("timeoff").innerHTML = (startTimer.userTime -  timerEventHandler.elapsedTime) + " minutes ";
+
+	var s = startTimer.userTime.split(" ",2);
+	
+	document.getElementById("timerText2").innerHTML = (s[0] -  timerEventHandler.elapsedTime + " minutes");
 
 	//If the timer is expired, stop music playback
-	if (timerEventHandler.elapsedTime >= startTimer.userTime ){
+	if (timerEventHandler.elapsedTime >= s[0] ){
 		window.location.href = "/alarm/sleep";
 		timerEventHandler.elapsedTime = 0;
 		window.clearInterval(timerEventHandler);
